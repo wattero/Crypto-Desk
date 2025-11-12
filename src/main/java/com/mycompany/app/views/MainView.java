f71197cb90f40d4b16268061c41236780dd77254
Onni Rinne <onni.rinne@tuni.fi>
2025-11-12 19:57:29 +0200

    clean up mainview

--- src/main/java/com/mycompany/app/views/MainView.java
+++ src/main/java/com/mycompany/app/views/MainView.java
@@ -1,35 +1,22 @@
 package com.mycompany.app.views;
 
-import com.mycompany.app.models.Crypto;
 import javafx.application.HostServices;
 import javafx.scene.layout.BorderPane;
-import java.util.function.Consumer;
 
 /**
  * Main application layout, holding the crypto list sidebar, detail view, and news view.
  */
 public class MainView extends BorderPane {
     private final NewsView newsView;
-    private final CryptoListView cryptoListView;
 
     public MainView(CryptoListView cryptoListView, CryptoDetailView detailView, NewsView newsView) {
         this.newsView = newsView;
-        this.cryptoListView = cryptoListView;
 
         setLeft(cryptoListView);
         setCenter(detailView);
         setRight(newsView);
 
         getStyleClass().add("main-view");
-    }
-
-    /**
-     * Set callback for when a crypto is selected (delegates to crypto list view)
-     */
-    public void setOnCryptoSelected(Consumer<Crypto> callback) {
-        if (cryptoListView != null) {
-            cryptoListView.setOnCryptoSelected(callback);
-        }
     }
 
     /**
